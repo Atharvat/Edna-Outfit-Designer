ce7177cfd7399de8627975b41e7ff7b442873976
Gunjan Dhanuka <d.gunjan@iitg.ac.in>
2023-08-13 12:26:54 +0530

    added colors and globals

---
+++ lib/globals/myColors.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color black = Color(0xFF1B2B32);
+  static const Color white = Colors.white;
+
+  // shades of blue
+  static const Color blue = Color(0xFF2568d8);
+  static const Color blueLighter = Color(0xFF387EDC);
+  static const Color blueLightest = Color(0xFFCEDEF6);
+
+  // shades of green
+  static const Color green20 = Color(0xFF027B4F);
+  static const Color green30 = Color(0xFF038969);
+  static const Color green10 = Color(0xFF20A090);
+
+  // shades of white
+  static const Color white30 = Color(0xFFC3D4E1);
+  static const Color white20 = Color(0xFFF2F7FB);
+  static const Color white10 = Color(0xFFFFFFFF);
+
+  // shades of black
+  static const Color black20 = Color(0xFF172033);
+  static const Color black30 = Color(0xFF0A111E);
+  static const Color black10 = Color(0xFF0A111E);
+
+  static const Color yellowAccent = Color(0xFFD49059);
+  static const Color gray = Color(0xFF818792);
+
+  // shades of red
+  static const Color red = Color(0xFFFA7367);
+  static const Color redLighter = Color(0xFFF2847F);
+  static const Color redLightest = Color(0xFFFBB8B2);
+
+  // shades of background color
+  static const Color backgroundColor = Color(0xFFF4F2F7);
+
+
+}
---
+++ lib/globals/myFonts.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyFonts{
+  Text heading(text, color){
+    return Text(text, style: TextStyle(
+      fontFamily: 'Playfair Display',
+      fontSize: 32,
+      fontWeight: FontWeight.w400,
+      color: color,
+    ));
+  }
+  Text message(text, color){
+    return Text(text, style: TextStyle(
+      fontFamily: 'General Sans Variable',
+      fontSize: 16,
+      height: 1.5,
+      letterSpacing: 0.24,
+      fontWeight: FontWeight.w500,
+      color: color,
+    ));
+  }
+  Text senderName(text, color){
+    return Text(text, style: TextStyle(
+      fontFamily: 'General Sans Variable',
+      fontSize: 14,
+      fontWeight: FontWeight.w500,
+      height: 1.43,
+      color: color,
+    ));
+  }
+}
