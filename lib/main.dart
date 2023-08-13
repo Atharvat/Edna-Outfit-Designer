acfd078edb50ef380c6e1dc39d325753664a15fa
Atharvat <tagalpallewaratharva@gmail.com>
2023-08-13 14:08:29 +0530

    added topbar for chat

--- lib/main.dart
+++ lib/main.dart
@@ -25,6 +25,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        fontFamily: 'General Sans'
       ),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
--- lib/screens/ChatScreen.dart
+++ lib/screens/ChatScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
@@ -9,21 +10,67 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
 
-  String pageTitle = "Goa Fashion Trip";
+  String pageTitle = "Goa fashion week";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(pageTitle),
+        primary: true,
+        titleSpacing: 0,
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: <Widget>[
+              IconButton(
+                icon: SvgPicture.asset("images/menu_icon.svg"),
+                tooltip: 'Menu',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
+              Text(pageTitle,
+                style: const TextStyle(
+                  fontFamily: 'Playfair Display',
+                  fontSize: 32,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              IconButton(
+                //add an image with rounded corners as an icon
+                icon: ClipRRect(
+                    borderRadius: BorderRadius.circular(100.0),
+                    child: const Image(
+                      image: AssetImage('images/gunjan.jpeg'),
+                      height: 32,
+                      width: 32,
+                    )// add an image with 32 height
+                ),
+                tooltip: 'Profile',
+                onPressed: () {
+                  // handle the press
+                },
+              ),
+            ]
+          ),
+        ),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
+            const Text(
               'Placeholder',
+            ),
+            Container(
+              child: const Text(
+                'You have pushed the button this many times:',
+              ),
             ),
           ],
         ),
